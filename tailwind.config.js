b264a5d49a43889a28802671ae230803ba24cdfd
Ayotayo1689 <badejopatrick7@gmail.com>
2025-02-18 17:59:58 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html', 
+    './src/**/*.{js,jsx,ts,tsx}', 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#041F62", 
+      },
+    },
+  },
+  plugins: [],
+};
+
